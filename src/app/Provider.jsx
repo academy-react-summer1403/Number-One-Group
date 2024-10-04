2df1433c89387375223a20f10acb0cebcf92cda4
Molaei <molaie.web.developer@gmail.com>
2024-10-04 21:19:38 +0100

    add react query config

--- src/app/Provider.jsx
+++ src/app/Provider.jsx
@@ -2,9 +2,10 @@ import { NextUIProvider } from "@nextui-org/react";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
 import store from "../redux/store";
+import { reactQueryConfig } from "../config/react-query";
 
 const ProviderApp = ({ children }) => {
-    const queryClient = new QueryClient();
+    const queryClient = new QueryClient(reactQueryConfig);
 
     return (
         <Provider store={store}>
