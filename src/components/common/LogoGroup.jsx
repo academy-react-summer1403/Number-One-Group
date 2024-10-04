049284ca464a84cd3cf6a078e4fcba40cbafafbe
Molaei <molaie.web.developer@gmail.com>
2024-10-04 05:32:40 +0100

    Add Logo Group

--- src/components/common/LogoGroup.jsx
+++ src/components/common/LogoGroup.jsx
@@ -5,7 +5,7 @@ const LogoGroup = ({ color }) => {
     const { t } = useTranslation();
     return (
         <Link className="w-auto flex gap-1 items-center">
-            <img src="../icons/logo.svg" alt="Logo" className="h-10" />
+            <img src="../logo.svg" alt="Logo" className="h-10" />
             <div className="mb-1 max-[1450px]:hidden max-[1270px]:block">
                 <h1 className={`text-xl text-nowrap font-semibold boldStyle_text ${color}`}>{t('nameGroup')}</h1>
                 <h2 className={`text-[10px] text-nowrap -mt-1 ${color}`}>{t('teamSlogan')}</h2>
