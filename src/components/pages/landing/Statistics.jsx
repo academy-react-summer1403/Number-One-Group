cae124655efdf9e07cdfab577ef6d9cb2a3ef466
Molaei <molaie.web.developer@gmail.com>
2024-10-10 21:34:00 +0100

    use react query

--- src/components/pages/landing/Statistics.jsx
+++ src/components/pages/landing/Statistics.jsx
@@ -1,26 +1,23 @@
 import { useTranslation } from "react-i18next"
 import CreateStatisticsItem from "./createStatisticsItem"
-import { LandingReportsItem } from "../../../core/constants/landing/Statistics"
-import { useEffect, useState } from "react"
-import GetLandingReports from "../../../core/services/api/get-data/GetLandingReports"
+import useGetReportsQuery from "../../../hooks/react-query/Reports"
 
 const Statistics = () => {
     const { i18n } = useTranslation()
-    const [reports, setReports] = useState([])
 
-    const getReports = async () => {
-        let reports = await GetLandingReports()
-        setReports(LandingReportsItem(reports))
-    }
-
-    useEffect(() => {
-        getReports()
-    }, [])
+    const { data, isSuccess } = useGetReportsQuery()
 
     return (
         <div className="w-full lg:px-44 sm:px-16 px-8 pb-28">
             <div className="w-full py-16 lg:py-20 xl:px-24 flex gap-y-16 flex-wrap md:flex-nowrap statistics_holder bg-DarkIndigo">
-                {reports.map(item => (<CreateStatisticsItem key={item.id} Statistics={item.statistics} name={i18n.language === "en" ? item.name[1] : item.name[0]} isBorder={item.isBorder} />))}
+                {isSuccess && (
+                    <>
+                        <CreateStatisticsItem name={i18n.language != "en" ? "وبلاگ ها" : "Blogs"} Statistics={data.newsCount} isBorder />
+                        <CreateStatisticsItem name={i18n.language != "en" ? "بهترین اساتید" : "Best Professors"} Statistics={data.teacherCount} isBorder />
+                        <CreateStatisticsItem name={i18n.language != "en" ? "دوره ها" : "Courses"} Statistics={data.courseCount} isBorder />
+                        <CreateStatisticsItem name={i18n.language != "en" ? "دانشجو های فعال" : "Active Students"} Statistics={data.studentCount} isBorder={false} />
+                    </>
+                )}
             </div>
         </div>
     )
