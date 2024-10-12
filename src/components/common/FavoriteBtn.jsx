33a7ce982a0a509d2d4bd04d4400899d7c734ab1
sobhan369arab <sobhanarab30@gmail.com>
2024-10-12 23:42:46 +0330

    create favorite button

---
+++ src/components/common/FavoriteBtn.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { UnCheckedHeartIcon, CheckedHeartIcon } from "../../core/icon";
+import { useMediaQuery } from "react-responsive";
+import { Skeleton, Tooltip } from "@nextui-org/react";
+import { useTranslation } from "react-i18next";
+import tooltipStyle from "../../core/constants/tooltip-style";
+
+const FavoriteBtn = ({ isLoading, variantStyle }) => {
+    const isTabletOrLapTop = useMediaQuery({ query: '(min-width: 768px)' })
+    const [isClick, setClick] = useState(false);
+    const variant = {
+        card: (
+            <Skeleton isLoaded={!isLoading} className={`rounded-lg ${location.search === '?V=2' && isTabletOrLapTop ? "" : "absolute left-6 top-6"}`}>
+                <div onClick={() => setClick(!isClick)} className={`w-[35px] h-[35px] flex justify-center items-center cursor-pointer favorite-btn bg-LightLavender rounded-xl`}>
+                    {isClick ? <CheckedHeartIcon width="25px" height="25px" /> : <UnCheckedHeartIcon width="25px" height="25px" fill="#555555" />}
+                </div>
+            </Skeleton>
+        ),
+        other: (
+            <div onClick={() => setClick(!isClick)} className="bg-LightLavender border-2 border-LightGrayish flex justify-center cursor-pointer items-center rounded-full w-[50px] h-[50px]">
+                {isClick ? <CheckedHeartIcon width="27px" height="27px" /> : <UnCheckedHeartIcon width="23px" height="23px" />}
+            </div>
+        )
+    }
+    const { i18n } = useTranslation()
+
+    return (
+        <Tooltip {...tooltipStyle} content={i18n.language == "en" ? "Add To Favorite" : "افزودن به علاقهمندی ها"}>
+            {variant?.[variantStyle]}
+        </Tooltip>
+    )
+}
+
+export default FavoriteBtn
--- src/components/common/sectionTop/ChangeView.jsx
+++ src/components/common/sectionTop/ChangeView.jsx
@@ -3,8 +3,8 @@ import { useLocation, useSearchParams } from "react-router-dom"
 import { useEffect } from "react";
 import { ButtonData } from "../../../core/constants/Courses/ChangeBtn_Data";
 import { Tooltip } from "@nextui-org/react";
-import toolTipStyle from "../../../core/constants/tooltip-style/tooltip"
 import { useTranslation } from "react-i18next";
+import tooltipStyle from "../../../core/constants/tooltip-style";
 
 const ChangeView = ({ setShowGrid }) => {
     const [searchParams, setSearchParams] = useSearchParams({ V: 1 })
@@ -28,7 +28,7 @@ const ChangeView = ({ setShowGrid }) => {
     return (
         <div className="md:flex hidden gap-2">
             {ButtonData.map((Button, index) => (
-                <Tooltip key={index} {...toolTipStyle} content={i18n.language == "en" ? Button.tooltip[1] : Button.tooltip[0]}>
+                <Tooltip key={index} {...tooltipStyle} content={i18n.language == "en" ? Button.tooltip[1] : Button.tooltip[0]}>
                     <div onClick={() => SearchParams(Button.id)} className={`${location.search == `?V=${Button.id}` ? "bg-VioletBlue" : "bg-transparent"} cursor-pointer border border-VioletBlue p-2 rounded-md`}>
                         <Button.icon stroke={location.search == `?V=${Button.id}` ? "#fff" : "#5751E1"} />
                     </div>
