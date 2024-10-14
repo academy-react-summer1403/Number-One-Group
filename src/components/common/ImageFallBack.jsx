bf6589918d5cdd984852fce0ff0689ecaa7fc41e
Molaei <molaie.web.developer@gmail.com>
2024-10-14 06:54:42 +0100

    fix bugs

--- src/components/common/ImageFallBack.jsx
+++ src/components/common/ImageFallBack.jsx
@@ -2,13 +2,11 @@ const ImageFallBack = ({ src, alt, fallback, ...props }) => {
 
     if (src == null) { src = fallback }
 
-    const handleError = () => src = fallback
-
     return (
         <img
             src={src}
             alt={alt}
-            onError={handleError}
+            onError={() => { return src = fallback }}
             {...props}
         />
     )
