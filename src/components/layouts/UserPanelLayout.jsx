22b9f53be979cdf256d916944600f14298b80ff7
Molaei <molaie.web.developer@gmail.com>
2024-10-20 15:02:53 +0100

    change user panel header

--- src/components/layouts/UserPanelLayout.jsx
+++ src/components/layouts/UserPanelLayout.jsx
@@ -29,8 +29,8 @@ const UserPanelLayout = () => {
     }
 
     return (
-        <div className='py-20 flex flex-wrap gap-y-10 sm:px-16 px-8'>
-            <UserPanelHeader />
+        <div className='py-10 flex flex-wrap gap-y-10 sm:px-16 px-8'>
+            {/* <UserPanelHeader /> */}
             <div className='relative w-full lg:h-[813px] flex rounded-2xl overflow-hidden sm:userPanel_holder_shadow'>
                 <MediaQuery minWidth="1280px">
                     <div className='min-w-[308px] w-[308px] bg-VioletBlue dark:bg-[#1B1B2A] flex justify-center flex-wrap py-12'>
@@ -44,13 +44,7 @@ const UserPanelLayout = () => {
                                 <SidebarUserPanel />
                             </HamburgerMenu>
                         </MediaQuery>
-                        <div className='flex gap-x-4 items-center'>
-                            <Tooltip {...tooltipStyle} content={i18n.language == "en" ? "Home" : "خانه"}>
-                                <Link to="/" className='cursor-pointer hover:scale-110 duration-200'>
-                                    <HomeBtnPanelIcon width="30" height="30" fill="#5751E1" />
-                                </Link>
-                            </Tooltip>
-                        </div>
+                        <UserPanelHeader />
                     </div>
                     <div className='w-full h-full py-14 flex justify-center'>
                         <Outlet />
