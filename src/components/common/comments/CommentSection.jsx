6d3b52ddeb3863a7892cb517bcdf7bea14d53d2b
Molaei <molaie.web.developer@gmail.com>
2024-10-21 03:54:43 +0100

    change style

--- src/components/common/comments/CommentSection.jsx
+++ src/components/common/comments/CommentSection.jsx
@@ -3,17 +3,17 @@ import { useTranslation } from "react-i18next";
 import AddComments from "./add-comments/AddComments";
 import UserComments from "./comment-box/UserComments";
 
-const CommentSection = ({ Id, replayComment,getReplay, apiFunction, data, refetch, variant }) => {
+const CommentSection = ({ Id, replayComment, getReplay, apiFunction, data, refetch, variant }) => {
   const { t } = useTranslation();
   const [viewMore, setViewMore] = useState(false);
   const dataRender = viewMore ? data : data?.slice(0, 3);
 
   return (
     <div className="my-3">
-      <h1 className="mb-6">{t('user_comment')}</h1>
+      <h1 className="mb-6 text-[18px]">{t('user_comment')}</h1>
       <AddComments apiFunction={apiFunction} Id={Id} refetch={refetch} />
       <p className="text-xl my-5">{data?.length} {t('comment')}</p>
-      <div className={`my-5 pb-10`}>
+      <div className="my-5 pb-10">
         {data?.length !== 0 ? (
           dataRender?.map((item, index) => (
             <UserComments
@@ -30,12 +30,15 @@ const CommentSection = ({ Id, replayComment,getReplay, apiFunction, data, refetc
               itemId={Id} />
           ))
         ) : (
-          <h1 className="mx-auto w-fit font-semibold text-xl">* {t('notFoundComment')} *</h1>
+          <h1 className="mx-auto mt-20 w-fit text-neutral-400">* {t('notFoundComment')} *</h1>
         )}
       </div>
-      <button onClick={() => setViewMore(!viewMore)} className={`bg-LavenderMist
-        px-5 py-1.5 rounded-3xl mx-auto block text-VioletBlue ${data?.length > 3 ? "" : "hidden"}`}>
-        {t(!viewMore ? 'viewMore' : 'viewLess')} </button>
+      <button
+        onClick={() => setViewMore(!viewMore)}
+        className={`bg-LavenderMist px-5 py-1.5 rounded-3xl mx-auto block text-VioletBlue ${data?.length > 3 ? "" : "hidden"}`}
+      >
+        {t(!viewMore ? 'viewMore' : 'viewLess')}
+      </button>
     </div>
   )
 }
