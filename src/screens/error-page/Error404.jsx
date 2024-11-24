0eea37511af38ff3931defb7d5edf771ef3e7154
Molaei <molaie.web.developer@gmail.com>
2024-11-24 17:46:18 +0330

    fix bug error page

--- src/screens/error-page/Error404.jsx
+++ src/screens/error-page/Error404.jsx
@@ -1,27 +1,7 @@
-import { useTranslation } from "react-i18next"
-import { CustomButton } from "../../components/common";
-import { BreadCrumb, TitleSection } from "../../components/partials/title-section";
-import { Error404Icon } from "../../core/icon";
-import { motion } from "framer-motion";
-import configVariants from "../../config/page-transition";
+import Error404Wrapper from "../../components/pages/error-404";
 
 const Error404 = () => {
-    const { t } = useTranslation();
-    return (
-        <motion.div
-            variants={configVariants}
-            initial={"initial"}
-            animate={"animate"}
-            exit={"exit"}
-        >
-            <div className="m-auto sm:my-16 my-12 w-fit flex flex-wrap boldStyle_text text-DarkBlue gap-y-4 justify-center cursor-default">
-                <Error404Icon className="sm:w-[420px] w-80" />
-                <h1 className="w-full text-center">{t('error')}!</h1>
-                <h1 className="w-full text-center">{t('ErrorDesc')}</h1>
-                <CustomButton arrowColor={"#fff"} vStyle="purple" vType="link" text={'GoHomePage'} />
-            </div>
-        </motion.div>
-    )
+    return <Error404Wrapper />;
 }
 
 export default Error404
