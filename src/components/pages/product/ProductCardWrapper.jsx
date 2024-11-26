6fb6d0db65d5d6a17ce68d9a032abd2731dea12e
Molaei <molaie.web.developer@gmail.com>
2024-11-27 01:11:56 +0330

    fix bug

--- src/components/pages/product/ProductCardWrapper.jsx
+++ src/components/pages/product/ProductCardWrapper.jsx
@@ -2,6 +2,7 @@ import ProductHeaderCard from './ProductHeaderCard'
 import ProductContentCard from './ProductContentCard'
 import ProductFooterCard from './ProductFooterCard'
 import { Link } from 'react-router-dom'
+import Tilt from 'react-parallax-tilt';
 
 const ProductCardWrapper = ({ item }) => {
     const {
@@ -16,18 +17,20 @@ const ProductCardWrapper = ({ item }) => {
     } = item
 
     return (
-        <Link
-            to={`/productDetails/${id}`}
-            className='w-[300px] h-fit flex flex-wrap gap-y-4 p-5 border border-LightGrayish rounded-lg hover:shadow-lg duration-200 relative cursor-pointer'
-        >
-            <ProductHeaderCard image={pictureList?.[0]} special={special} />
-            <ProductContentCard
-                category={categoryId}
-                exist={exist}
-                title={title}
-            />
-            <ProductFooterCard discount={discount} price={price} />
-        </Link>
+        <Tilt transitionSpeed={2500}>
+            <Link
+                to={`/productDetails/${id}`}
+                className='w-[300px] h-fit flex flex-wrap gap-y-4 p-5 border border-LightGrayish rounded-lg hover:shadow-lg duration-200 relative cursor-pointer'
+            >
+                <ProductHeaderCard image={pictureList?.[0]} special={special} />
+                <ProductContentCard
+                    category={categoryId}
+                    exist={exist}
+                    title={title}
+                />
+                <ProductFooterCard discount={discount} price={price} />
+            </Link>
+        </Tilt>
     )
 }
 
