02036f92a3488bc7d8e1a8e285834969ff243d33
sobhan369arab <sobhanarab30@gmail.com>
2024-11-28 12:15:46 +0330

    create picture box

--- src/components/pages/shop-details/index.jsx
+++ src/components/pages/shop-details/index.jsx
@@ -4,6 +4,9 @@ import { BreadCrumb, TitleSection } from "../../partials/title-section"
 import { useQueryWithDependencies } from "../../../core/hooks/react-query";
 import GetShopDetails from "../../../core/services/api/get-data/GetShopDetails";
 import { motion } from "framer-motion";
+import fallback from "../../../assets/images/image-not-found.png"
+import { ImageFallBack } from "../../common";
+
 
 const ShopDetailsWrapper = () => {
   const { id } = useParams();
@@ -21,6 +24,13 @@ const ShopDetailsWrapper = () => {
         <BreadCrumb href={'/shops'} text={'ShopSection'} />
         <BreadCrumb type="Div" text={SelectedShop?.name} />
       </TitleSection>
+      <div className="xl:w-9/12 w-[88%]  mx-auto my-20 relative  cursor-default">
+        <ImageFallBack
+          src={SelectedShop?.img}
+          className="w-full md:h-[450px] rounded-xl"
+          fallback={fallback}
+        />
+      </div>
 
     </motion.div>
   )
