edde1d3bd2c4807a7b16a131a2852aa16a49fead
sobhan369arab <sobhanarab30@gmail.com>
2024-12-04 21:44:09 +0330

    fix bug

--- src/components/common/bottom-nav/HelperButton.jsx
+++ src/components/common/bottom-nav/HelperButton.jsx
@@ -1,5 +1,6 @@
 
 import { useTour } from "@reactour/tour"
+import { useEffect } from "react";
 import { TbHelpOctagonFilled } from "react-icons/tb";
 import { useLocation } from "react-router-dom";
 
@@ -7,6 +8,10 @@ const HelperButton = () => {
   const location = useLocation()
   const locStatus = location.pathname === '/userPanel' || location.pathname === '/UserPanel';
   const { setIsOpen } = useTour()
+
+  useEffect(() => {
+    setIsOpen(true)
+  }, [])
 
   return (
     <button className={`bottomNav ${locStatus ? "" : "hidden"} `} onClick={() => setIsOpen(true)}> <TbHelpOctagonFilled size={25} /></button>
