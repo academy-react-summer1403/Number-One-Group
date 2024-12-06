d29420bcc9cf82f388e608bbab0baa00ee0a5901
Molaei <molaie.web.developer@gmail.com>
2024-12-06 04:02:43 +0330

    add skeleton

--- src/components/pages/product/ProductContentCard.jsx
+++ src/components/pages/product/ProductContentCard.jsx
@@ -1,8 +1,9 @@
+import { Skeleton } from '@nextui-org/react'
 import { useQueryWithDependencies } from '../../../core/hooks/react-query'
 import { GetProductCategory } from '../../../core/services/api/get-data'
 import { Label } from '../../common'
 
-const ProductContentCard = ({ title, category, exist }) => {
+const ProductContentCard = ({ title, category, exist, loading }) => {
     // Get Category For Products
     const { data, isSuccess } = useQueryWithDependencies(
         "GET_PRODUCT_CATEGORY",
@@ -13,16 +14,18 @@ const ProductContentCard = ({ title, category, exist }) => {
 
     return (
         <div className='w-full border-b border-gray-400/30 pb-4 flex flex-wrap gap-y-4'>
-            <h1 className='text-DarkBlue text-xl'>{title}</h1>
-            <div className='w-full flex justify-between'>
-                <Label variant={'bgBlue'} text={isSuccess && data.categoryName} style='px-6 py-1' />
-                <div className='flex gap-x-2 items-center h-full'>
-                    <div className='mediumStyle_text flex gap-1'>
-                        <span className='text-VioletBlue'>{exist}</span>
-                        <span>موجود</span>
+            <Skeleton isLoaded={!loading} className='w-full'>
+                <h1 className='w-full text-DarkBlue text-xl'>{title}</h1>
+                <div className='w-full flex justify-between'>
+                    <Label variant={'bgBlue'} text={isSuccess && data.categoryName} style='px-6 py-1' />
+                    <div className='flex gap-x-2 items-center h-full'>
+                        <div className='mediumStyle_text flex gap-1'>
+                            <span className='text-VioletBlue'>{exist}</span>
+                            <span>موجود</span>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Skeleton>
         </div>
     )
 }
--- src/components/pages/product/ProductFooterCard.jsx
+++ src/components/pages/product/ProductFooterCard.jsx
@@ -1,14 +1,17 @@
+import { Skeleton } from "@nextui-org/react"
 import { SeparationPrice, UnitPrice } from "../../../core/utility/SeparationPrice"
 
-const ProductFooterCard = ({ discount, price }) => {
+const ProductFooterCard = ({ discount, price, loading }) => {
     return (
-        <div className='w-full flex justify-between mediumStyle_text'>
-            <span className='line-through font-Pop_Med'>{SeparationPrice(discount)}</span>
-            <div>
-                <span className='text-VioletBlue text-lg ml-1'>{UnitPrice(price)}</span>
-                <span>تومان</span>
+        <Skeleton isLoaded={!loading} className="w-full">
+            <div className='w-full flex justify-between mediumStyle_text'>
+                <span className='line-through font-Pop_Med'>{SeparationPrice(discount)}</span>
+                <div>
+                    <span className='text-VioletBlue text-lg ml-1'>{UnitPrice(price)}</span>
+                    <span>تومان</span>
+                </div>
             </div>
-        </div>
+        </Skeleton>
     )
 }
 
