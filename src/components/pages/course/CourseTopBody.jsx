170c67af3790b493b46905b6a139b4a079bdf774
Molaei <molaie.web.developer@gmail.com>
2024-12-06 17:32:58 +0330

    fix bug

--- src/components/pages/course/CourseTopBody.jsx
+++ src/components/pages/course/CourseTopBody.jsx
@@ -18,7 +18,7 @@ const CourseTopBody = ({
         <h1 className='text-lg line-clamp-1 mt-2 course-title text-DarkBlue group-hover/item:text-VioletBlue duration-150'>{title && title}</h1>
       </Skeleton>
       <div className='flex items-center justify-between my-2 '>
-        <div className={`flex gap-1 ${location.search === '?V=1' ? "w-40" : ""}  ${category?.length > 2 ? "scrollBehavior" : ""}`}>
+        <div className={`w-40 flex gap-1 ${location.search === '?V=1' ? "w-40" : ""}  ${category?.length > 2 ? "scrollBehavior" : ""}`}>
           {categoryList?.map((categoryBox, index) => (
             <Skeleton key={index} isLoaded={!isLoaded} className="pb-1 rounded-lg">
               <Label key={index} variant={'bgTextGray'} text={categoryBox} style='text-xs whitespace-nowrap ' />
